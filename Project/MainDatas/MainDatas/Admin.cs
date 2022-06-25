0dfdb21861afdb4c7c0b98e2107eafed7748d7f4
HengamehBaghiyan <hengamehb1382@gamil.com>
2022-06-25 18:25:17 +0430

    kamel kardan kart banki

--- Project/MainDatas/MainDatas/Admin.cs
+++ Project/MainDatas/MainDatas/Admin.cs
@@ -4,14 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text.RegularExpressions;
 
 namespace MainDatas
 {
     public class Admin
     {
-        public static int mojoodi_froshgah;
+        static public ObservableCollection<string> emails = new ObservableCollection<string>();
+        public static int mojoodi_froshgah=0;
         public ObservableCollection<Bank_Card> bank_Cards = new ObservableCollection<Bank_Card>();
         public string Password;
+        public string Email;
+        public bool Modir;
+        readonly Regex Emailcheck = new Regex(@"^([\w\.\-]{1,32})@([\w\-]{1,32})((\.(\w){1,32})+)$");
+        readonly Regex Passcheck = new Regex(@"^(?=.*?[A-Z])(?=.*?[a-z]).{8,40}");
 
+        public Admin(string email,string password,bool modir,bool x)
+        {
+            if (emails.Contains(email))
+                throw new Exception("This email has already token");
+            if (!Emailcheck.IsMatch(email))
+                throw new Exception("Invalid email address");
+            if (!Passcheck.IsMatch(password))
+                throw new Exception("Invalid password");
+            Email = email;
+            Password = password;
+            Modir = modir;
+            if (x)
+            {
+                SqlConnection put = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\win_10\BookStore\Project\MainDatas\MainDatas\data\admindata.mdf; Integrated Security = True; Connect Timeout = 30");
+                put.Open();
+                string command = "Insert into Customers values('" + email.Trim() + "','" + Password.Trim() + "','" + 0.0 + "') ";
+                SqlCommand doo = new SqlCommand(command, put);
+                doo.BeginExecuteNonQuery();
+                put.Close();
+            }
+        }
     }
 }
