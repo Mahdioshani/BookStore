9cf218ce700ba3314d84e52c4391add0d4f0f307
HengamehBaghiyan <hengamehb1382@gamil.com>
2022-07-04 13:07:49 +0430

    sql Admin

--- Project/MainDatas/MainDatas/Admin.cs
+++ Project/MainDatas/MainDatas/Admin.cs
@@ -10,11 +10,11 @@ namespace MainDatas
     {
         public static float gheymat_vip {get; set;}
         public static int rooz_vip { get; set;}
-        static public ObservableCollection<string> emails = new ObservableCollection<string>();
-        static public ObservableCollection<Admin> admins = new ObservableCollection<Admin>();
+        static public ObservableCollection<string> emails = new ObservableCollection<string>();//
+        static public ObservableCollection<Admin> admins = new ObservableCollection<Admin>();//
         public static float mojoodi_froshgah = 0;
         string password;
-        public string Password
+        public string Password//
         {
             get { return password; }
             set
@@ -24,27 +24,27 @@ namespace MainDatas
                 else { throw new Exception("Invalid Password"); }
             }
         }
-        public string Email { get; set; }
-        public bool Modir;
+        public string Email { get; set; }//
 
-        readonly Regex Emailcheck = new Regex(@"^([\w\.\-]{1,32})@([\w\-]{1,32})((\.(\w){1,32})+)$");
-        readonly Regex Namecheck = new Regex(@"^[A-Za-z]{3,32}$");
-        readonly Regex Phonenumbercheck = new Regex(@"^(09)[0-9]{9}$");
-        readonly Regex Passcheck = new Regex(@"^(?=.*?[A-Z])(?=.*?[a-z]).{8,40}");
+        readonly Regex Emailcheck = new Regex(@"^([\w\.\-]{1,32})@([\w\-]{1,32})((\.(\w){1,32})+)$");//
+        readonly Regex Namecheck = new Regex(@"^[A-Za-z]{3,32}$");//
+        readonly Regex Phonenumbercheck = new Regex(@"^(09)[0-9]{9}$");//
+        readonly Regex Passcheck = new Regex(@"^(?=.*?[A-Z])(?=.*?[a-z]).{8,40}");//
         private string firstname;
         private string lastname;
         private String phonenum;
-        public string Firstname
+        public string Firstname//
         {
             get { return firstname; }
             set
             {
                 if (Namecheck.IsMatch(value))
                     firstname = value;
-                else { throw new Exception("Invalid FirstName"); }
+                else { throw new Exception("Invalid FirstName");}
+
             }
         }
-        public string Lastname
+        public string Lastname//
         {
             get { return lastname; }
             set
@@ -54,7 +54,7 @@ namespace MainDatas
                 else { throw new Exception("Invalid LastName"); }
             }
         }
-        public string Phonenumber
+        public string Phonenumber//
         {
             get { return this.phonenum; }
             set
@@ -87,19 +87,19 @@ namespace MainDatas
             {
                 string a = Convert.ToString(table.Rows[i][0]);
                 string b = Convert.ToString(table.Rows[i][1]);
-                bool c = Convert.ToBoolean(table.Rows[i][2]);
-                float d = (float)table.Rows[i][3];
-                string h = Convert.ToString(table.Rows[i][4]);
-                Admin help = new Admin(a, b, c, false);
-                if (d != null)
-                    Admin.mojoodi_froshgah = d;
-
+                string c = table.Rows[i][2].ToString();
+                string d = table.Rows[i][3].ToString();
+                string e = table.Rows[i][4].ToString();
+                Admin help = new Admin(a, b, false);
+                help.Firstname = c;
+                help.Lastname = d;
+                help.Phonenumber = d;
             }
             SqlCommand command = new SqlCommand(extract, connection);
             command.ExecuteNonQuery();
             connection.Close();
         }
-        public Admin(string email, string password, bool modir, bool x)
+        public Admin(string email, string password, bool x)
         {
             if (emails.Contains(email))
                 throw new Exception("This email has already token");
@@ -109,14 +109,13 @@ namespace MainDatas
                 throw new Exception("Invalid password");
             Email = email;
             Password = password;
-            Modir = modir;
             admins.Add(this);
             emails.Add(email);
             if (x)
             {
                 SqlConnection put = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\karen\Documents\GitHub\BookStore\Project\MainDatas\MainDatas\data\booksdata.mdf;Integrated Security=True;Connect Timeout=30");
                 put.Open();
-                string command = "Insert into alladmin values('" + email.Trim() + "','" + Password.Trim() + "','" + modir + "','" + mojoodi_froshgah + "') ";
+                string command = "Insert into alladmin (Email,Password) Values('" + email.Trim() + "','" + Password.Trim() + "') ";
                 SqlCommand doo = new SqlCommand(command, put);
                 doo.BeginExecuteNonQuery();
                 put.Close();
